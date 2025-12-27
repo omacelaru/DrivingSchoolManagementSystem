6ced84ad83c83c2ffd1601f2abd08bb319c6ea31
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2025-12-27 15:38:02 +0200

    [IMPROVE] Moved GatewayConfig to config package

--- api-gateway/src/main/java/com/drivingschool/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/drivingschool/gateway/config/GatewayConfig.java
@@ -1,4 +1,4 @@
-package com.drivingschool.gateway;
+package com.drivingschool.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
